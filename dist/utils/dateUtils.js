f6c04449064dd3beed5063464183f055e260e9ef
somadevfat <somahiranodev@gmail.com>
2025-08-24 15:52:26 +0900

    chore(build): ビルド成果物と依存ディレクトリの追跡を解除（.gitignore に準拠）

--- dist/utils/dateUtils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCurrentISODate = getCurrentISODate;
-function getCurrentISODate() {
-    return new Date().toISOString();
-}
